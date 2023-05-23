d74c6cc14e5ca114d6908053da58d437a50e4953
Jordan Uhe <82374826+Jordan-Uhe@users.noreply.github.com>
2023-05-23 17:06:52 +1000

    get -> cookie, score, songlist, results

--- Playing.php
+++ Playing.php
@@ -2,6 +2,7 @@
 <html lang="en">
 
 <head>
+    <script src='cookie.js' type='text/javascript'></script>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,17 +13,27 @@
     function myFunction() {
         let x = document.getElementById("player-input").value;
         let text;
-        if (x != "y") {
-            text = "Input not Correct";
+        const musiclist = [
+            ["fUYaosyR4bE", "Lily Allen", "Not Fair"],
+            ["NI6aOFI7hms", "The Cardigans", "Lovefool"],
+            ["ryDOy3AosBw", "Skee-Lo", "I Wish"],
+            ["fPO76Jlnz6c", "Coolio", "Gangsta's Paradise"],
+            ["aSLZFdqwh7E", "Eminem", "Stan"],
+            ["34Na4j8AVgA", "The Weeknd", "Starboy"],
+            ["fnlJw9H0xAM", "NF", "The Search"],
+            ["S9bCLPwzSC0", "Eminem", "Mockingbird"],
+            ["VDvr08sCPOc", "Fort Minor", "Remember The Name"],
+            ["dvgZkm1xWPE", "Coldplay", "Viva La Vida"]
+        ];
+        if (x != musiclist[readCookie("song")][2]) {
+            text = "Wrong";
 
         } else {
-            text = "Input Correct";
-            console.log("test");
+            text = "Correct";
             const attr = document.createAttribute("disabled");
             document.getElementById("player-input").setAttributeNode(attr);
-            const attr2 = document.createAttribute("disabled");
-            document.getElementById("check-answer").setAttributeNode(attr2);
-            // score++;
+            createCookie("score", readCookie("score") * 1 + 1, 1);
+
 
         }
         document.getElementById("result").innerHTML = text;
@@ -33,8 +44,26 @@
     function onLoad() {
         console.log("on load");
 
+if (readCookie("song") >=10 ) {
+    console.log("redirect");
+    location.replace("results.php");
+};
+
+        const musiclist = [
+            ["fUYaosyR4bE", "Lily Allen", "Not Fair"],
+            ["NI6aOFI7hms", "The Cardigans", "Lovefool"],
+            ["ryDOy3AosBw", "Skee-Lo", "I Wish"],
+            ["fPO76Jlnz6c", "Coolio", "Gangsta's Paradise"],
+            ["aSLZFdqwh7E", "Eminem", "Stan"],
+            ["34Na4j8AVgA", "The Weeknd", "Starboy"],
+            ["fnlJw9H0xAM", "NF", "The Search"],
+            ["S9bCLPwzSC0", "Eminem", "Mockingbird"],
+            ["VDvr08sCPOc", "Fort Minor", "Remember The Name"],
+            ["dvgZkm1xWPE", "Coldplay", "Viva La Vida"]
+        ];
         const attr3 = document.createAttribute("src");
-        attr3.value = "https://www.youtube.com/embed/CN7JSoGLaJE?autoplay=1&start=5&end=35";
+        attr3.value = "https://www.youtube.com/embed/" + musiclist[readCookie("song")][0] +
+            "?autoplay=1&start=5&end=35";
         const element = document.getElementById("youtube-frame");
         element.setAttributeNode(attr3);
 
@@ -42,43 +71,29 @@
 </script>
 
 <body>
+    <a href="index.php" target="_self">home</a>
     <div class="container">
         <div class="song-guessr">
-            <div class="status-bar"></div>
-            <div class="previous"> </div>
-
             <div class="playing">
-                <div onload=>
-
-                    <iframe width="300" height="300" id="youtube-frame" frameborder="0"></iframe>
-                    <form action="start.php" method="get">
-                        <input type="hidden" id="mood" name="mood" value=<?php print_r($_GET['mood']) ?>>
-                        <input type="hidden" id="mode" name="mode" value=<?php print_r($_GET['mode']) ?>>
-                        <input type="hidden" id="song" name="song" value=<?php print_r($_GET['song'] + 1) ?>>
-
-                        <input id="player-input"><br>
-
-                        <button type="button" id="check-answer" onclick="myFunction()">Check Answer
-
-                        <p id="result"></p> <br>
-
-
-                        </button><input type="submit" value="Submit"> next song <button type="submit"><br></button>
-                        <br>
-
-
-                    </form>
-
-                </div>
-
+                <iframe width="300" height="300" id="youtube-frame" frameborder="0"></iframe>
+                <input id="player-input"><br>
+                <p id="result"></p> <br>
             </div>
-            <div class="score">
-
+            <div class="options">
+                <button type="radio" onclick="myFunction()">
+                    <img src="image/check.png" style="width:130px; margin-right:20px;">
+                </button>
+                <a href="playing.php">
+                    <button type="radio" onclick='createCookie("song", readCookie("song") *1  +1, 1)'>
+                        <img src="image/next.png" style="width:130px; margin-right:20px;">
+                    </button>
+                </a>
             </div>
-            <div class="buzzerd">
 
 
-            </div>
+
+
+
         </div>
     </div>
 </body>
@@ -91,12 +106,8 @@
         <hr>
         <h3>Debug Area</h3>
         <pre>
-GET Contains:
-<?php print_r($_GET) ?>
-
-POST Contains:
-<?php print_r($_POST) ?>
-
+        COOKIE Contains:
+<?php print_r($_COOKIE) ?>
   </pre>
     </aside>
 </section>
--- mood.php
+++ mood.php
@@ -1,66 +1,64 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
+    <script src='cookie.js' type='text/javascript'></script>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="song.css">
 </head>
+
 <body>
-<div class="container">
-    <div class="song-guessr">
-        <div class="status-bar">
-            <img src="image/Status Bar.png">
-        </div>
-        <div class="moods">
-            <img src="image/Moods.png" style="width:75px; margin-left:95px; margin-top: 15px;">
-        </div>
-        <div class="previous">
-            <a href="song.html">
-                <button type="submit">
-                    <img src="image/previous.png" width="50px">
-                </button>
-            </a>
-        </div>
-        
-        <div class="mood1">
-            <a href="mode.php?mood=random">
-                <button type="submit">
-                    <img src="image/mood_random.png" style="width:130px; margin-right:20px;">
-                </button>
-            </a>
-            <a href="mode.php?mood=party">
-                <button type="submit">
-                    <img src="image/mood_party.png" width="130px">
-                </button>
-            </a>
-        </div>
-        <div class="mood2">
-            <a href="mode.php?mood=workout">
-                <button type="submit">
-                    <img src="image/mood_workout.png" style="width:130px; margin-right:20px;">
-                </button>
-            </a>
-            <a href="mode.php?mood=chill">
-                <button type="submit">
-                    <img src="image/mood_chill.png" width="130px">
-                </button>
-            </a>
+    <a href="index.php" target="_self">home</a>
+    <div class="container">
+        <div class="song-guessr">
+            <div class="moods">
+                <img src="image/Moods.png" style="width:75px; margin-left:95px; margin-top: 15px;">
+            </div>
+            <div class="previous">
+                <a href="index.php">
+                    <button type="submit">
+                        <img src="image/previous.png" width="50px">
+                    </button>
+                </a>
+            </div>
+
+            <div class="mood1">
+                <a href="playing.php">
+                    <button type="submit" onclick='createCookie("mood", "random", 1); createCookie("song", 0, 1); createCookie("score", 0, 1)'>
+                        <img src="image/mood_random.png" style="width:130px; margin-right:20px;">
+                    </button>
+                </a>
+                <a href="playing.php">
+                    <button type="submit" onclick='createCookie("mood", "party", 1); createCookie("song", 0, 1); createCookie("score", 0, 1)'>
+                        <img src="image/mood_party.png" width="130px">
+                    </button>
+                </a>
+            </div>
+            <div class="mood2">
+                <a href="playing.php">
+                    <button type="radio" onclick='createCookie("mood", "workout", 1); createCookie("song", 0, 1); createCookie("score", 0, 1)' >
+                        <img src="image/mood_workout.png" style="width:130px; margin-right:20px;" >
+                    </button>
+                </a>
+                <a href="playing.php">
+                    <button type="radio" onclick='createCookie("mood", "chill", 1)' >
+                        <img src="image/mood_chill.png" width="130px">
+                    </button>
+                </a>
+            </div>
         </div>
     </div>
-</div>
 </body>
 <section>
     <aside id="debug">
         <hr>
         <h3>Debug Area</h3>
         <pre>
-GET Contains:
-<?php print_r($_GET) ?>
-
-POST Contains:
-<?php print_r($_POST) ?>
+        COOKIE Contains:
+<?php print_r($_COOKIE) ?>
 
   </pre>
     </aside>
